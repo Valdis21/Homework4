543f65b03830cb12336ffffe9f3747db5da5bfd3
Vladislav <vladkkon@mail.ru>
2024-03-11 20:23:49 +0300

    Начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int age1 = 18;
+        int age2 = 16;
+        if (age1 >= 18) {
+            System.out.println("Если возраст человека равен " + age1 + " он совершеннолетний.");
+        } else {
+            System.out.println("Если возраст человека равен " + age1 + " он не достиг совершеннолетия, нужно немного подождать.");
+        }
+        if (age2 >= 18) {
+            System.out.println("Если возраст человека равен " + age2 + " он совершеннолетний.");
+        } else {
+            System.out.println("Если возраст человека равен " + age2 + " он не достиг совершеннолетия, нужно немного подождать.");
+        }
+
+
+    }
+}
